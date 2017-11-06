5368e64c6bf5e4bda13d7b6981b969298473ba30
Anil Kumar <panilkumar16@gmail.com>
2017-11-06 12:54:35 -0800

    refactor: Add new file ListShelves to have Parent updates Child
    Why: To have React design patterns

--- src/App.js
+++ src/App.js
@@ -1,21 +1,79 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
-import ListBooks from './ListBooks'
+import { Route, Link } from 'react-router-dom'
+import ListShelves from './ListShelves'
 import SearchBooks from './SearchBooks'
 import './App.css'
+import * as BooksAPI from './BooksAPI'
 
-// Using Stateless Function as this is only rendering without state
-function BooksApp () {
-  return (
-    <div className="app">
-      <Route exact path="/" render={() => (
-        <ListBooks/>
-      )}/>
-      <Route path="/search" render={() => (
-        <SearchBooks/>
-      )}/>
-    </div>
-  )
+class BooksApp extends React.Component {
+
+  state = {
+    currentlyReading: [],
+    wantToRead: [],
+    read: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({
+        currentlyReading: books.filter(book => book.shelf === "currentlyReading"),
+        wantToRead: books.filter(book => book.shelf === "wantToRead"),
+        read: books.filter(book => book.shelf === "read")
+      })
+    })
+  }
+
+  updateShelfs = (book, bookShelf) => {
+    console.log(bookShelf)
+    //let oldShelf = book.shelf
+    BooksAPI.update(book, bookShelf)
+    BooksAPI.getAll().then((books) => {
+      this.setState({
+        currentlyReading: books.filter(book => book.shelf === "currentlyReading"),
+        wantToRead: books.filter(book => book.shelf === "wantToRead"),
+        read: books.filter(book => book.shelf === "read")
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Route exact path="/" render={() => (
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                <ListShelves
+                  shelfTitle="Currently Reading"
+                  shelfBooks={this.state.currentlyReading}
+                  onUpdateBook={this.updateShelfs}
+                />
+                <ListShelves
+                  shelfTitle="Want to Read"
+                  shelfBooks={this.state.wantToRead}
+                  onUpdateBook={this.updateShelfs}
+                />
+                <ListShelves
+                  shelfTitle="Read"
+                  shelfBooks={this.state.read}
+                  onUpdateBook={this.updateShelfs}
+                />
+              </div>
+            </div>
+            <div className="open-search">
+              <Link to="/search" className="open-search">Add a book</Link>
+            </div>
+          </div>
+        )}/>
+        <Route path="/search" render={() => (
+          <SearchBooks/>
+        )}/>
+      </div>
+    )
+  }
 }
 
 export default BooksApp
